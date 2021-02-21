5d6fb33bd7048a62fb1fa9c7081ab603bfb2841b
nadia800 <61513773+nadia800@users.noreply.github.com>
2021-02-21 20:40:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Test } from "./Test";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
